ae6fbe7104a7e4899c10555a9c8618ad92375385
Laxmi kant <laxmikant.pal@investwellonline.com>
2022-05-30 16:08:07 +0530

    added all

--- investwell/src/main/java/com/libs/investwell/AppConstant.kt
+++
@@ -1,4 +0,0 @@
-package com.libs.investwell
-
-class AppConstant {
-}
---
+++ investwell/src/main/java/com/libs/investwell/ToasterMessage.kt
@@ -0,0 +1,10 @@
+package com.libs.investwell
+
+import android.content.Context
+import android.widget.Toast
+
+object ToasterMessage {
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
