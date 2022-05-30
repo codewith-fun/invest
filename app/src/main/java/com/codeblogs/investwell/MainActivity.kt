2392eac6494c6727a467fa17b6e83268dc33f74f
Laxmi kant <laxmikant.pal@investwellonline.com>
2022-05-30 16:33:48 +0530

    added all test

--- app/src/main/java/com/codeblogs/investwell/MainActivity.kt
+++ app/src/main/java/com/codeblogs/investwell/MainActivity.kt
@@ -2,11 +2,13 @@ package com.codeblogs.investwell
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.libs.investwell.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToasterMessage.s(this,"pagal")
 
     }
 }
